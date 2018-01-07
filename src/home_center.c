22f8d145c473712186d3babbe2182ace99eec0ee
lovelycrabs <happyboy18@126.com>
2018-01-07 21:17:16 +0800

    add common

--- src/home_center.c
+++ src/home_center.c
@@ -10,9 +10,35 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include "globe.h"
 
-int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
-	printf("update");
+#ifdef MSWIN32
+int init_net(){
+    WSADATA wsaData;
+    return WSAStartup(MAKEWORD(2,2), &wsaData);
+
+}
+int destroy_net(){
+	WSACleanup();
+}
+#endif
+
+int main(int argc,char **args) {
+	#ifdef MSWIN32
+	if(init_net()){
+		printf("WSAStartup fail!\r\n");
+		exit(EXIT_FAILURE);
+
+	}
+	#endif
+
+	printf("start success!\r\n");
+
+
+
+	#ifdef MSWIN32
+	destroy_net();
+	#endif
+
 	return EXIT_SUCCESS;
 }
